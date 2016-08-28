82bc0e63b401bf5c345d54ac68cb65ff66eea2eb
FrostLife07 <skyfrost.07@gmail.com>
2016-08-28 23:53:46 +0700

    second commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Template</title>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+        <script src="js/jquery-3.1.0.min.js"></script>
+
+    </head>
+    <body>
+        <header id="header">
+            <nav class="navbar navbar-light bg-faded">
+                <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#bsmenu" aria-expanded="false">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#">Admin Panel</a>
+                <div class="collapse navbar-toggleable-xs" id="bsmenu">
+                    <ul class="nav navbar-nav pull-md-right">
+                        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-home"></i> Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-bell"></i> Notify</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#"><i class="fa fa-sign-in"></i> Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Register</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <section id="main_body">
+
+        </section>
+
+        <footer id="footer">
+
+        </footer>
+
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
